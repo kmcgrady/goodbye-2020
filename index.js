ff41235aaa38fafafd418d94866b298cb2d8d07c
Kevin Meldau <me@kevinmeldau.com>
2021-01-04 12:49:04 -0500

    Event delegation

--- index.js
+++ index.js
@@ -9,15 +9,27 @@ $(function () {
     $progress.animate({ width: yearPercentage + "%" });
   }
   setProgress();
-  let $increamentButton = $("#increment");
-  $increamentButton.click(function () {
-    currentDay += 10;
-    setProgress();
-  });
 
-  let $decreamentButton = $("#decrement");
-  $decreamentButton.click(function (event) {
-    currentDay -= 10;
+  //   let $increamentButton = $("#increment");
+  //   $increamentButton.click(function () {
+  //     currentDay += 10;
+  //     setProgress();
+  //   });
+
+  //   let $decreamentButton = $("#decrement");
+  //   $decreamentButton.click(function (event) {
+  //     currentDay -= 10;
+  //     setProgress();
+  //   });
+
+  //   $decreamentButton.off();
+  let $pageWrapper = $("#page-wrapper");
+  $pageWrapper.on("click", "button", function (event) {
+    if (event.target.id === "increment") {
+      currentDay += 10;
+    } else if (event.target.id === "decrement") {
+      currentDay -= 10;
+    }
     setProgress();
   });
 });
